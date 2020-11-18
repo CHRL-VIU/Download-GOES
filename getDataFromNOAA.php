65c1fd8b6da3dac9bf6a60ef07fe8adcb6e84331
Alex Cebulski <Alex.Cebulski@viu.ca>
2020-11-18 14:13:30 -0800

    init

---
+++ getDataFromNOAA.php
@@ -0,0 +1,58 @@
+<?php
+require 'config.php';
+
+$msg = 'C:/LRGSClient/bin/getDcpMessages.bat';
+$fields = "DateTime, RH, Temp, Mx_Spd, Mx_Dir, WSK10mMax, WDD10mMax, Wspd, Dir, Rn_1, RnTotal, SDepth, SDcomp, SDist_Q, BP, Telem, Vtx, TCase, SM, ST, SWUavg15m, SWLavg15m, LWUavg15m, LWLavg15m, ALBavg15m, TA, SW, SD, PC, VB, Ib, Vs, I_S, YB";
+
+$conn = mysqli_connect(MYSQLHOST, MYSQLUSER, MYSQLPASS, MYSQLDB);
+
+if (!file_exists($msg)) {
+        echo "File not found. Make sure you specified the correct path.\n";
+        exit;
+}
+
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  exit;
+}
+
+
+/* Add redirection so we can get stderr. */
+
+$output = shell_exec( "C:/LRGSClient/bin/getDcpMessages -h \"cdadata.wcda.noaa.gov\" -u \"".NOAAUSER."\" -P \"".NOAAPASS."\" -f \"C:/LRGSClient/MessageBrowser.sc\" -b \"$$$\"");
+$array = explode("$$$", $output);
+
+foreach ($array as $line) {
+    if($line == ""){
+        continue;
+    }
+    //$lineArray = preg_split("/\r\n|\n|\r/", $line);
+    $lineArray = preg_split('/[\s]+/', $line);
+
+    // grab elements needed for date
+    $hr = substr($lineArray[0], 13, 2);
+    $yr = "20" . substr($lineArray[0], 8, 2);
+    $jday = substr($lineArray[0], 10, 3);
+    // $date = jdtogregorian(gregoriantojd(01, 01, $yr)+($jday-1));
+    // $datetime = date("Y-m-d H:00:00", strtotime($date . $hr)); 
+    $datetime = date("Y-m-d H:00:00", strtotime('+'.$jday.' days', mktime($hr, 0, 0, 1, 0, $yr)));  // LHS is jdays to add, RHS is reference time to add jdays to.
+
+    // remove first element of array
+    $removedItem = array_shift($lineArray);
+    // add datetime to start of array
+    array_unshift($lineArray, $datetime);
+    // remove empty line at end of array 
+    array_pop($lineArray);
+
+    print_r($lineArray);
+    // create string from array elements in current line
+    $datString = implode("','", $lineArray);
+
+    $query = "insert ignore into `raw_cruickshank` ($fields) values('$datString')";
+
+    if (!mysqli_query($conn, $query)) {
+    exit("Insert Query Error description: " . mysqli_error($conn));
+    }
+}
+
+?>
